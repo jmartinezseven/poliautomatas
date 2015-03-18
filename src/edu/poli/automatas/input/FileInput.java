ad9ec676502dded8b60a6d315291f4bc3b4a4158
Unitcom Digital <desarrollo@unitcomdigital.com>
2015-03-18 16:08:01 -0500

    Cargar Archivo
    No se ubica el archivo de lectura :(

--- src/edu/poli/automatas/input/FileInput.java
+++ src/edu/poli/automatas/input/FileInput.java
@@ -1,7 +1,30 @@
 package edu.poli.automatas.input;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by jsmartinez on 18/03/2015.
  */
 public class FileInput {
+
+    public static void leerArchivo(String archivo)throws FileNotFoundException,IOException {
+        String datos ;
+        FileReader f = new FileReader(archivo);
+        BufferedReader b = new BufferedReader(f);
+
+        while((datos = b.readLine())!=null){
+            System.out.print(datos);
+        }
+        b.close();
+    }
+
+    public static void main(String[] args)throws IOException{
+        leerArchivo("/src/edu/poli/automatas/input/DocumentoEntrada.txt");
+    }
+
+
+
 }
