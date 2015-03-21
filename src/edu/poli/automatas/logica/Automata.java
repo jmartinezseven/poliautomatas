7fef867d1be5cc8b7270bf7e4f6dcce9bee72d49
jmartinezseven <juanmartinez@seven4n.com>
2015-03-20 19:00:56 -0500

    metodos para setear estado inicial y estados de aceptacion

--- src/edu/poli/automatas/logica/Automata.java
+++ src/edu/poli/automatas/logica/Automata.java
@@ -7,17 +7,45 @@ import java.util.List;
  */
 public class Automata {
 
-    private List<Estado> estados;
     private String descripcion;
     private String alfabeto;
+    private List<Estado> estados;
 
+    /**
+     * Define los estados inicales y finales
+     * @param estadoInicial
+     * @param estadosAceptaion
+     */
+    public void definirEstadosInicialYAcepacion(String estadoInicial, List<String> estadosAceptaion) {
+        for(Estado estado: estados) {
+            if(estado.getNombre().equals(estadoInicial)) {
+                estado.setInicial(true);
+            } else {
+                String nombre = estado.getNombre();
+                if(estadosAceptaion.contains(nombre)){
+                    estado.setAceptacion(true);
+                }
+            }
+        }
+    }
 
-    private Estado
+    /**
+     * Retorna el estado inicial del automata
+     * @return
+     */
+    private Estado darEstadoInicial() {
+        Estado inicial = null;
+        for(Estado estado : estados){
+            if(estado.isAceptacion()){
+                inicial = estado;
+                break;
+            }
+        }
+        return inicial;
+    }
 
-
-
-    public void simulacion() {
-
+    public String simulacion() {
+        null;
     }
 
 }
