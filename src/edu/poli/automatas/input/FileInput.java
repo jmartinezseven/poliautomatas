f424a5b77e7b76dc3832a442c84c0d5d45bf1c65
jmartinezseven <juanmartinez@seven4n.com>
2015-03-20 19:31:40 -0500

    inicio de entradapara automata

--- src/edu/poli/automatas/input/FileInput.java
+++ src/edu/poli/automatas/input/FileInput.java
@@ -1,29 +1,20 @@
 package edu.poli.automatas.input;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by jsmartinez on 18/03/2015.
  */
 public class FileInput {
 
-    public static void leerArchivo(String archivo)throws FileNotFoundException,IOException {
-        System.out.print("fsdgsdf");
-//        String datos ;
-//        FileReader f = new FileReader(archivo);
-//        BufferedReader b = new BufferedReader(f);
-//
-//        while((datos = b.readLine())!=null){
-//            System.out.print(datos);
-//        }
-//        b.close();
-    }
-
     public static void main(String[] args)throws IOException{
-        leerArchivo("DocumentoEntrada.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        while ( line != null ) {
+            String descripcion = line;
+            String alfabeto = reader.readLine();
+            
+        }
     }
 
 
